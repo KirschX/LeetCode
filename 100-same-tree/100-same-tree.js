2280eb3582ad1c9ff36616ecfcac9743155a8cd8
KirschX <68739701+KirschX@users.noreply.github.com>
2022-02-07 16:18:15 +0900

    Time: 94 ms (46.14%), Space: 41.6 MB (17.37%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,48 +12,7 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    
-   
-    
-    let pArr =[];
-    let qArr =[];
-    let result;
-    
-    function checkSame (node) {
-        if (!node) {
-           pArr.push(null)
-            return;
-        }
-        
-        pArr.push(node.val)
-        checkSame(node.left)
-        checkSame(node.right)
-  
-    }
-    
-     function checkSame2 (node) {
-        if (!node) {
-           qArr.push(null)
-            return;
-        }
-        
-        qArr.push(node.val)
-        checkSame2(node.left)
-        checkSame2(node.right)
-  
-    }
-    
-    
-    checkSame(p);
-    checkSame2(q);
-    
-    console.log(pArr, qArr)
-    
-    if (JSON.stringify(pArr) === JSON.stringify(qArr)) {
-        result = true
-    } else {
-        result = false
-    }
-    
-    return result
-}
+    if (!p && !q) return true;
+    if (!p || !q || p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
