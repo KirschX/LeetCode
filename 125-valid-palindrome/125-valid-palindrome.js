97ca660f4edcca81b34276a72d674dfd914cd0ad
KirschX <68739701+KirschX@users.noreply.github.com>
2022-02-13 01:19:17 +0900

    Time: 88 ms (79.79%), Space: 46.1 MB (23.86%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+     s = s.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    
+   if (s === (s.split("").reverse().join(""))) return true
+    else return false
+    
+};
