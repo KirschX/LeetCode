23efebbab39d07e883112a1d77a9ef752be65a3a
KirschX <68739701+KirschX@users.noreply.github.com>
2022-02-13 18:45:19 +0900

    Time: 115 ms (47.63%), Space: 47 MB (13.02%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -4,15 +4,29 @@
  */
 var singleNumber = function(nums) {
     
-    let stack = [];
+//     let stack = [];
     
-    for(let i =0; i<nums.length; i++) {
-        if(stack.includes(nums[i])) {
-            stack.splice(stack.indexOf(nums[i]), 1)
-        } else {
-            stack.push(nums[i])
-        }
+//     for(let i =0; i<nums.length; i++) {
+//         if(stack.includes(nums[i])) {
+//             stack.splice(stack.indexOf(nums[i]), 1)
+//         } else {
+//             stack.push(nums[i])
+//         }
         
+//     }
+//     return stack[0]
+    
+    let obj = {};
+    
+    for(let i = 0; i<nums.length; i++) {
+        
+        if(`${nums[i]}` in obj) {
+            delete obj[nums[i]];
+        } else {
+            obj[nums[i]] = 1;
+        }
+            // console.log(obj, nums[i])
     }
-    return stack[0]
+    
+    return Object.keys(obj)[0]
 };
