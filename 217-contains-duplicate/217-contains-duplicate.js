5f242db224175e0a74c4e66ba32f3e18ef305bd8
KirschX <68739701+KirschX@users.noreply.github.com>
2022-02-17 22:15:27 +0900

    Time: 2152 ms (7.47%), Space: 49.9 MB (35.97%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,16 +3,31 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const set = new Set();
+//     const set = new Set();
+    
+//     for(let i =0; i<nums.length; i++){
+        
+        
+//         if(set.has(nums[i])) {
+//             return true;
+//         }
+        
+//         set.add(nums[i])
+//         // console.log(set)
+//     }
+    
+//     return false
+    
+    const set = [];
     
     for(let i =0; i<nums.length; i++){
         
         
-        if(set.has(nums[i])) {
+        if(set.includes(nums[i])) {
             return true;
         }
         
-        set.add(nums[i])
+        set.push(nums[i])
         // console.log(set)
     }
     
