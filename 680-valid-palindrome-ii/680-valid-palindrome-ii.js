aca63d8a3aa2bb8b9ce53aaacf6a72de1767d066
KirschX <68739701+KirschX@users.noreply.github.com>
2022-02-18 18:10:43 +0900

    Time: 121 ms (54.20%), Space: 48.4 MB (25.34%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.js
@@ -6,12 +6,8 @@ var validPalindrome = function(s) {
 
     function check(string) {
         
-        // if (string == string.split("").reverse().join("")) {
-        //     return true;
-        // } else return false;
-        
         for(let i=0; i<Math.floor(string.length/2); i++) {
-            // console.log(string[i], string[string.length-1-i])
+
             if(string[i] !== string[string.length-1-i]) {
                 return false;
             }
@@ -25,19 +21,15 @@ var validPalindrome = function(s) {
         if(s[i] !== s[s.length-1-i]) {
             let a = s.slice(i+1, s.length-i)
             let b = s.slice(i, s.length-1-i)
-        
-            // console.log(a)
+
             if(check(a)|| check(b)) {
                 return true;
             } else return false;
             
         }
         
-        
     }
         
-
-
     
     return true;
 };
