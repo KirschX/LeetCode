2d9c78c1ebc16f5cdc401e7c0d51ed51bd3ffb72
KirschX <68739701+KirschX@users.noreply.github.com>
2022-02-21 16:30:11 +0900

    Time: 72 ms (88.29%), Space: 46.2 MB (7.89%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,7 +4,46 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    if(nums.includes(target)){
-        return nums.indexOf(target)
-    } else return -1
+
+
+    let count = 0;
+    
+    let start = 0;
+    
+    let end = nums.length-1;
+    
+    let mid = [];
+    
+    mid = Math.floor(nums.length / 2);
+    
+    if(nums.length <= 2) {
+        if(nums.includes(target)){
+            return nums.indexOf(target)
+        }
+        else return -1;
+    }
+    
+    while(end-start !== 2){
+        
+           console.log(mid, nums[mid], end - start)
+          
+            if (nums[mid] > target) {
+                end = mid;
+                mid = Math.floor(end / 2)
+           
+            } else if (nums[mid] < target) {
+                start = mid;
+                mid = Math.floor((end + start) / 2)
+          
+            } else {
+                
+                return nums.indexOf(target)
+            }
+        
+  
+          }
+    
+    return nums.indexOf(target)
+
+    
 };
