029cc37539d172482bf1174a21343dd8826a7163
KirschX <68739701+KirschX@users.noreply.github.com>
2022-03-03 15:01:44 +0900

    Time: 106 ms (64.12%), Space: 45 MB (6.85%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,6 +3,36 @@
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function(nums, target) {
+
+//     let result;
+    
+
+//     let map = new Map();
+    
+//     nums.map((item) => {
+        
+//         if(item === target-item) {
+//                  if(nums.indexOf(item , nums.indexOf(item) +1) !== -1) {
+//                      result = [nums.indexOf(item) , nums.indexOf(item,  nums.indexOf(item) +1)];
+//                      return result;
+//                  }
+//         }
+        
+//         if(map.get(item) == target-item) {
+//          result = [nums.indexOf(target-item), nums.indexOf(item)]
+//         }
+//         map.set(target-item, item)
+        
+//     })
+    
+
+
+            
+    
+// return result;
+
+
 var twoSum = function(nums, target) {
 
     let result;
@@ -10,30 +40,12 @@ var twoSum = function(nums, target) {
 
     let map = new Map();
     
-    nums.map((item) => {
-        
-        if(item === target-item) {
-                 if(nums.indexOf(item , nums.indexOf(item) +1) !== -1) {
-                     result = [nums.indexOf(item) , nums.indexOf(item,  nums.indexOf(item) +1)];
-                         // console.log(item , nums.indexOf(item,  nums.indexOf(item) +1))
-              
-                     return result;
-                 }
-            
-    
-        }
+    nums.map((item, index) => {
         
         if(map.get(item) == target-item) {
-
-        // console.log(nums.indexOf(target-item), nums.indexOf(item))
-         result = [nums.indexOf(target-item), nums.indexOf(item)]
-            return result;
+         result = [nums.indexOf(target-item), index]
         }
-        
-        // if(! (Math.abs(item) > Math.abs(target)) )
         map.set(target-item, item)
-        
-//   console.log(map)
         
     })
     
