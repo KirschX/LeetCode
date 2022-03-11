fcba0a90bae3b0c1cc4cf1c59de88f9a13f3e8f4
KirschX <68739701+KirschX@users.noreply.github.com>
2022-03-11 12:53:53 +0900

    Time: 136 ms (62.06%), Space: 45.9 MB (35.99%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    
+    let map = new Map();
+    
+    let result;
+    
+    for(let i =0; i<s.length; i++) {
+        if(map.has(s[i])){
+            map.set(s[i], map.get(s[i])+1)
+            
+        } else {
+             map.set(s[i], 1)    
+        }
+       // console.log(map)
+    }
+    
+    // for(let i of map.values()){
+    //     if (i === 1) return i;
+    // }
+    // console.log(map.values())
+    
+    let arr = Array.from(map.keys())
+    
+    // console.log(arr)
+    
+    for(let i =0; i<arr.length; i++){
+        if (map.get(arr[i]) === 1) {
+            console.log(arr[i])
+            return s.indexOf(arr[i])
+            }
+       
+    }
+
+    return -1
+        
+    
+  
+        
+};
+
+/// map에서 value가 1인 key값을 찾아서 s에서의 key값으 위치를 찾아라
