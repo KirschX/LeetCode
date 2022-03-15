0144f45dd9b4323a5f4b0d00473094a7ccfa7e45
KirschX <68739701+KirschX@users.noreply.github.com>
2022-03-15 19:50:54 +0900

    Time: 56 ms (96.05%), Space: 42.4 MB (10.67%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var reformatDate = function(date) {
+    
+    let mt = {
+        "Jan" : 01,
+        "Feb" : 02,
+        "Mar" : 03,
+        "Apr" : 04,
+        "May" : 05,
+        "Jun" : 06,
+        "Jul" : 07,
+        "Aug" : 08,
+        "Sep" : 09,
+        "Oct" : 10,
+        "Nov" : 11,
+        "Dec" : 12
+    }
+    
+
+    let [day, month, year] = date.split(" ")
+    
+    day = parseInt(day.slice(0,-2)) > 9 ? day.slice(0,-2) : "0" + day.slice(0,-2)
+    
+    month = parseInt(mt[month]) >9 ? mt[month] : "0" + mt[month]
+    
+    year = parseInt(year)
+    
+    // console.log(year, month, day)
+
+    
+    return (`${year}-${month}-${day}`)
+
+};
