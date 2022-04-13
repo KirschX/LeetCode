80cf5772fa65760285f31d7567d132a5177ba79b
KirschX <68739701+KirschX@users.noreply.github.com>
2022-04-13 19:22:34 +0900

    Time: 80 ms (96.84%), Space: 45.2 MB (89.69%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -5,55 +5,32 @@
 var lengthOfLongestSubstring = function(s) {
     
     
-    // abcabcbb
-    // 12323321
-    
-    // abba
-    // 121
-    
-    // abbca
-    // 12123
-    //   !
-   // 1212
-    
-    // pwwkew
-    // 121231
-    
-    //wobgrovw
-    //12345456
-    //w의위치가 start 포인트보다 큰지 작은지
-    
-    //pwwkew
-    //121233
-    
-    if(!s.length) return 0
-    
-    let count = 0;
-    let max = 1;
-    let map = new Map();
     let start = 0;
+    let L = 0;
+    let map = new Map()
+    let max = 0;
     
-    for(let i=0; i<s.length; i++){
+    for(let i =0; i<s.length; i++){
         
+       
         
-             
-        if( !map.has(s[i]) ){
-            count++;
-           } 
+        if(!map.has(s[i])){
+            L++;
+        }
         else {
-           start = Math.max(start, map.get(s[i]) + 1)
+            start = Math.max(map.get(s[i]) +1, start)
+            L =  Math.min(i - start + 1)
             
-           count = Math.min(i - start +1 , i - map.get(s[i])) 
-             
+            
         }
         
         map.set(s[i], i)
-        max = Math.max(max, count)
         
-//         console.log(count, max,  start, map)  
-    
+        max = Math.max(max, L)
         
+        // console.log(L, max)
     }
+    
     
     return max
 };
