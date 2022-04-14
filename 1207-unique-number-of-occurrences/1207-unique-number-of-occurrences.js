53134a9e415dc4ecb99eb2715ead0f52d5075b57
KirschX <68739701+KirschX@users.noreply.github.com>
2022-04-15 01:39:52 +0900

    Time: 70 ms (71.68%), Space: 42.6 MB (66.73%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function(arr) {
+    let map = new Map();
+    
+    arr.map((item,index) => {
+            map.set(item, map.get(item) + 1 || 1)
+    })
+    
+    // console.log(map)
+    
+    let occArr = Array.from(map.values())
+    let sOccArr = [...new Set(occArr)]
+    
+    if( occArr.length == sOccArr.length ) return true;
+    else return false;
+};
