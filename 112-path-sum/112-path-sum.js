c578d478e1d52aa2f56c038f43c30a694d5f257c
KirschX <68739701+KirschX@users.noreply.github.com>
2022-04-25 00:06:16 +0900

    Time: 105 ms (35.48%), Space: 45.7 MB (47.39%) - LeetHub

---
+++ 112-path-sum/112-path-sum.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    
+    let result;
+    
+    function trav(node, sum) {
+        if (!node) return;
+        
+
+
+        sum = sum + node.val;
+        if(!node.left && !node.right && sum === targetSum) result = true;
+//         console.log(sum)
+       
+        trav(node.left, sum)
+        trav(node.right, sum)
+    }
+    
+    trav(root, 0)
+    
+    return  result || false
+};
+
+//          7
+//        0   n
+//     -1  -6
+//    n 1 n  n
+//     n -7
