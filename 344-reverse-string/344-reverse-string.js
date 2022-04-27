ef9cbb60f7370689f7806cdddc57ae62f3ddd335
KirschX <68739701+KirschX@users.noreply.github.com>
2022-04-27 22:13:08 +0900

    Time: 140 ms (31.94%), Space: 49.4 MB (49.26%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,6 +3,11 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    
-    s = s.reverse().join("").split("")
+  let i = 0, j = s.length - 1;
+  while (i < j) {
+    // ES6 destructuring assignment
+    [s[i], s[j]] = [s[j], s[i]];
+    i++;
+    j--;
+  }
 };
