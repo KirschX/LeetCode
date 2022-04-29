c8902e9c9941ac2161fd157dc8c03252b940db71
KirschX <68739701+KirschX@users.noreply.github.com>
2022-04-29 17:27:00 +0900

    Time: 154 ms (14.27%), Space: 44.9 MB (37.92%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    
+    let map = new Map()
+    let map2 = new Map()
+    
+    for(let i = 0; i<s.length; i++) {
+        if(!map.has(s[i])) {
+            map.set(s[i], 1)
+        } else {
+            map.set(s[i], map.get(s[i]) + 1)
+        }
+    }
+    
+    for(let i=0; i<t.length; i++){
+        if(!map2.has(t[i])) {
+            map2.set(t[i], 1)
+        } else {
+            map2.set(t[i], map2.get(t[i]) + 1)
+        }
+    }
+    
+    
+    console.log(map, map2)
+    
+    if(map.size !== map2.size) return false
+    
+    for(i of map.keys()){
+        if(map.get(i) !== map2.get(i)) return false
+    }
+
+    return true
+};
