087f6097bb5d47fa47542231753e2f81f377e3f6
KirschX <68739701+KirschX@users.noreply.github.com>
2022-05-03 21:09:01 +0900

    Time: 154 ms (21.94%), Space: 51.4 MB (70.89%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
@@ -1,8 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
 const removeDuplicates = s => {
-  const arr = s.split('');
-  let ans = 0;
-  for (let i = 0; i < arr.length; ++i) {
-    ans === 0 || arr[i] !== arr[ans - 1] ? (arr[ans++] = arr[i]) : --ans;
-  }
-  return arr.slice(0, ans).join('');
+    
+    let stack = [];
+    
+    for(let i=0; i<s.length; i++){
+        if(stack[stack.length-1] === s[i]) {
+            stack.pop()
+        } else {
+            stack.push(s[i])
+        }
+        
+        // console.log(stack)
+    }
+    
+    return stack.join("")
+    
 };
