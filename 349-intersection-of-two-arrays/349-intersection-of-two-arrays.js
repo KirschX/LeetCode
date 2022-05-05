a92659d34fc8c2d9e350351569ad3929f1d13ad2
KirschX <68739701+KirschX@users.noreply.github.com>
2022-05-05 23:15:14 +0900

    Time: 81 ms (51.60%), Space: 43.5 MB (53.10%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -3,21 +3,8 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersection = function(nums1, nums2) {
-    let map = new Map();
-    let map2 = new Map();
-    let ans = []
-    
-    nums1.map((item, i) => {
-        if(!map.has(nums1[i])){
-            map.set(nums1[i], 1)    
-        }
-    })
-    
-     nums2.map((item, i) => {
-        if(map.has(nums2[i]) && !ans.includes(nums2[i])){
-            ans.push(nums2[i])
-        }
-    })
-    return ans
-};
+function intersection (nums1 , nums2){
+    let setNum1 = new Set(nums1);
+
+    return [...new Set(nums2.filter(num => setNum1.has(num)))];
+}
