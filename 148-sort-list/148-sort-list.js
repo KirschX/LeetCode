1f874d49d9a349b250b4707c55f40e56d3157fc9
HyeonWook Jung <68739701+KirschX@users.noreply.github.com>
2022-05-17 22:24:24 +0900

    Time: 301 ms (18.67%), Space: 71.5 MB (5.09%) - LeetHub

---
+++ 148-sort-list/148-sort-list.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    let H = head;
+    let temp = [];
+    
+    while(head){
+        temp.push(head.val)
+        head = head.next;
+    }
+    
+    temp = temp.sort((a,b) => a-b)
+    // console.log(temp)
+    
+    let newHead = new ListNode();
+    let NH = newHead;
+    
+    temp.map((item,i) => {
+        newHead.next = new ListNode(item)
+        newHead = newHead.next;
+    })
+    
+    return NH.next
+};
