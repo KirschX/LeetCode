c8b9695785fc0dbb60403c9a79c70df77f14582f
HyeonWook Jung <68739701+KirschX@users.noreply.github.com>
2022-05-21 19:46:52 +0900

    Time: 77 ms (91.46%), Space: 45.9 MB (48.05%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -6,45 +6,19 @@
  */
 
 var topKFrequent = function(nums, k) {
-//     let obj = {};
-//     let result = [];
-    
-//     nums.map(item => {
-//         obj[item] =(obj[item] +1 || 1)
-//     })
-    
-//     let sorted = Object.entries(obj)
-//     .sort((a,b) => a[1] - b[1])
-    
-    
-//     while(result.length !== k) {
-//         result.push(sorted.pop()[0])
-//     }
-    
-    
-//     // console.log(obj)
-//     // console.log(sorted)
-    
-
-//     return result
-    
     const result =[];
     const map = new Map();
     
     nums.map(item => map.set(item, map.get(item) + 1 || 1))
     
-    // console.log(map)
-    
     let sorted = Array.from(map.entries())
                .sort((a,b) => a[1] - b[1])
      
-    // console.log(sorted)
         while(result.length !== k) {
         result.push(sorted.pop()[0])
     }
     
     return result
-    
 };
 
 
