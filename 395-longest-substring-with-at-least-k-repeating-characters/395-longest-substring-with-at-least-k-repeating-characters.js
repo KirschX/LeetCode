736a91b99e56fd3f03b084f1abd7e57ee45670dd
HyeonWook Jung <68739701+KirschX@users.noreply.github.com>
2022-05-22 22:36:56 +0900

    Time: 253 ms (31.25%), Space: 49.4 MB (14.58%) - LeetHub

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+// 0540 ~ 
+
+const longestSubstring = function(s, k) {
+
+    
+
+    function DAQ(left, right) {
+        // console.log(left, right)
+        if(right - left < k) return 0;
+        let map = new Map();
+        for(let i = left; i < right; i++){
+            map.set(s[i], map.get(s[i]) +1 || 1)      
+        }
+        
+        for(let pivot = left; pivot < right; pivot++){
+            if(map.get(s[pivot]) < k) {
+                let nextPivot = pivot + 1;
+                while(nextPivot < right && map.get(s[nextPivot]) <k) {
+                    nextPivot++;
+                    console.log(pivot,nextPivot)
+                }
+                return Math.max(DAQ(left, pivot), DAQ(nextPivot, right))
+            }
+            
+        }
+        return right - left
+    }
+
+    return DAQ(0, s.length)
+
+};
+    
+    
+// aaabb 3
+
+
+// aacacbbb 3
+// aaabbc 3
+// aaabbabcb 3
+// ababbc 2
+// abbaaac 3
+        
+// ababcb
+
