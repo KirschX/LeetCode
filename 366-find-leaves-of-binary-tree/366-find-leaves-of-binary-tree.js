44b1f8504c9cffce13a60c8f4ca36785ff9476a3
HyeonWook Jung <68739701+KirschX@users.noreply.github.com>
2022-06-21 01:31:52 +0900

    Time: 104 ms (14.95%), Space: 42.8 MB (10.59%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.js
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.js
@@ -1,15 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
 var findLeaves = function(root) {
-    const res = {};
-    const dfs = (node) => {
-        if(!node) return 0;
-        const depth = Math.max(dfs(node.left), dfs(node.right));
-        if(res[depth]) {
-            res[depth].push(node.val);
-        } else {
-            res[depth] = [node.val];
+    
+    let result = [];
+    let temp = [];
+    
+    function dfs(node) {
+        if(!node) return null;
+
+        if(!node.left && !node.right) {
+            temp.push(node.val)
+            return null;
         }
-        return depth + 1;
+
+        node.left = dfs(node.left)
+        node.right = dfs(node.right)
+        
+        return node;
     }
-    dfs(root);
-    return Object.values(res);
+    
+    while(root){
+        root = dfs(root)
+        result.push(temp);
+        temp = [];
+        if(!root) break;
+    }
+    
+
+    // console.log(result, temp)
+    // console.log(root)
+    
+
+    return result;
 };
+
+
+// dfs 순회 - leaf val을 temp에 넣고 초기화 - while문 돌기(조건: ) - 
