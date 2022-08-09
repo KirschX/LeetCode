89c7b9e2e434294f2678b1da9f276d58d56b4ba2
HyeonWook Jung <68739701+KirschX@users.noreply.github.com>
2022-08-09 20:26:33 +0900

    Time: 68 ms (86.08%), Space: 42.5 MB (46.84%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.js
+++ 1496-path-crossing/1496-path-crossing.js
@@ -1,27 +1,23 @@
-/**
- * @param {string} path
- * @return {boolean}
- */
+const coordToStr = ([x, y]) => x + ':' + y; // construct string from x and y
+
 var isPathCrossing = function(path) {
-        let pos = [0,0]
-        let result = false;
-        let set = new Set()
-        set.add(JSON.stringify([0,0]))
-    for(let i =0; i<path.length; i++){
-        
-        if(path[i] === "N"){
-            pos[1] ++;
-        } else if (path[i] === "S"){
-            pos[1] --;
-        } else if (path[i] === "W"){
-            pos[0] --;
-        } else if(path[i] === "E"){
-            pos[0] ++;
-        }
-        if(set.has(JSON.stringify(pos))) result = true;
-        set.add(JSON.stringify(pos))
-        // console.log(set)
+    let dict = {
+        'N': [0, 1],
+        'S': [0, -1],
+        'E': [1, 0],
+        'W': [-1, 0],
     }
-    
-    return result;
+    let prev = [0, 0];
+    let map = new Map();
+    map.set(coordToStr(prev), 1); // initialise object with start position coordinates
+    for (let i = 0; i < path.length; i++) {
+        let direction = dict[path[i]]; 
+        let newCoord = [prev[0] + direction[0], prev[1] + direction[1]]; // calculate new coordinate
+        let newCoordKey = coordToStr(newCoord);
+        map.set(newCoordKey, (map.get(newCoordKey) || 0) + 1); // increase count of x + y string coordinate
+        if (map.get(newCoordKey) > 1) return true; // if there are two or more of the same coordinates then we hit our previous path
+        prev = newCoord; // store new coordinate to use it for calculation in the next iteration
+    // console.log(map)
+    }
+    return false;
 };
