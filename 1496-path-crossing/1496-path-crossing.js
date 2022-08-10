b41cabd8a6fb86c8ccbd2e8f27fdd4c660b448c0
HyeonWook Jung <68739701+KirschX@users.noreply.github.com>
2022-08-10 16:31:12 +0900

    Time: 204 ms (6.33%), Space: 47.9 MB (5.06%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.js
+++ 1496-path-crossing/1496-path-crossing.js
@@ -1,23 +1,34 @@
-const coordToStr = ([x, y]) => x + ':' + y; // construct string from x and y
-
+/**
+ * @param {string} path
+ * @return {boolean}
+ */
 var isPathCrossing = function(path) {
-    let dict = {
-        'N': [0, 1],
-        'S': [0, -1],
-        'E': [1, 0],
-        'W': [-1, 0],
+        let pos = [0,0];
+        let map = new Map();
+        let result = false;
+    
+    // function toString (arr) {
+    //     return `${arr[0]}${arr[1]}`
+    // }
+        map.set(JSON.stringify(pos), (map.get(JSON.stringify(pos)) || 0) + 1)
+      
+    for(let i =0; i<path.length; i++){
+        
+        if(path[i] === "N"){
+            pos[1] ++;
+        } else if (path[i] === "S"){
+            pos[1] --;
+        } else if (path[i] === "W"){
+            pos[0] --;
+        } else if(path[i] === "E"){
+            pos[0] ++;
+        }
+        map.set(JSON.stringify(pos), (map.get(JSON.stringify(pos)) || 0) + 1)
+        // console.log(map)
+        if(map.get(JSON.stringify(pos)) >= 2) {
+            result = true;
+        }
     }
-    let prev = [0, 0];
-    let map = new Map();
-    map.set(coordToStr(prev), 1); // initialise object with start position coordinates
-    for (let i = 0; i < path.length; i++) {
-        let direction = dict[path[i]]; 
-        let newCoord = [prev[0] + direction[0], prev[1] + direction[1]]; // calculate new coordinate
-        let newCoordKey = coordToStr(newCoord);
-        map.set(newCoordKey, (map.get(newCoordKey) || 0) + 1); // increase count of x + y string coordinate
-        if (map.get(newCoordKey) > 1) return true; // if there are two or more of the same coordinates then we hit our previous path
-        prev = newCoord; // store new coordinate to use it for calculation in the next iteration
-    // console.log(map)
-    }
-    return false;
+    
+    return result;
 };
