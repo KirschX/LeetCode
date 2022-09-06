93fd28cfb45cc1b1ea3e1995a26472f63e0229c0
HyeonWook Jung <68739701+KirschX@users.noreply.github.com>
2022-09-06 16:01:58 +0900

    Time: 168 ms (62.89%), Space: 45.5 MB (82.61%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.js
@@ -5,36 +5,73 @@
  */
 var findAnagrams = function(s, p) {
     
-    let left = 0;
     let result = [];
+    let left = 0;
+    let right = 0;
     let map = new Map()
-    let pMap = new Map()
+    let count = p.length;
     
-    for (let i=0; i<p.length; i++){
-       pMap.set(p[i], (pMap.get(p[i]) || 0) + 1)
+    for(let i=0; i<p.length; i++){
+        map.set(p[i], (map.get(p[i]) || 0) + 1)
     }
-    pMap = Array.from(pMap).sort()
     
-   // console.log(pMap)
-    
-  
-    
-    for (let right = 0; right<s.length; right++){
-        map.set(s[right], (map.get(s[right]) || 0) + 1)
+    while(right < s.length){
+        if(map.get(s[right]) > 0) count--;
         
-        if(right> p.length-1){
-            map.set(s[left], map.get(s[left]) - 1)
-            if(map.get(s[left]) === 0) map.delete(s[left])
-            left++;
+        
+        map.set(s[right], (map.get(s[right]) || 0) - 1)
+         right++;
+        
+        if(count === 0) result.push(left)
+        
+        if(right-left === p.length){
+                if(map.get(s[left]) >= 0) count++;
+                map.set(s[left], (map.get(s[left]) || 0)  + 1)
+                left ++;
         }
-        // console.log(map)
-             let temp = Array.from(map).sort()
-        if(JSON.stringify(temp) === JSON.stringify(pMap)) result.push(left)
-        map = new Map(temp)
-       
         
-        // console.log(result)
+        // console.log(map, left, right, count)
     }
-    
+   
     return result
 };
+
+
+// const findAnagrams = (s, p) => {
+    
+
+//     const output = [];
+//     const neededChars = {};
+    
+
+//     for (let char of p) {
+//         if (char in neededChars) {
+//             neededChars[char]++
+//         } else neededChars[char] = 1
+//     }
+
+//     let left = 0;
+//     let right = 0; 
+//     let count = p.length 
+    
+
+//     while (right < s.length) {
+     
+//         if (neededChars[s[right]] > 0) count--;
+        
+
+//         neededChars[s[right]]--;
+//         right++;
+        
+     
+//         if (count === 0) output.push(left);
+        
+
+//         if (right - left == p.length) {
+//             if (neededChars[s[left]] >= 0) count++;
+//             neededChars[s[left]]++;
+//             left++;
+//         }
+//     }
+//     return output;
+// };
