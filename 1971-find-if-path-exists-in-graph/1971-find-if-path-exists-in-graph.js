24313666e7b0663bf8c26dfceb0605c04daf7251
HyeonWook Jung <68739701+KirschX@users.noreply.github.com>
2022-09-13 16:07:28 +0900

    Time: 880 ms (34.90%), Space: 162.1 MB (41.48%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -4,26 +4,26 @@ var validPath = function(n, edges, start, end) {
     let stack = [start];
     
     // build adjacency list
-//     edges.forEach(edge => {
-//         let v = edge[0], e = edge[1];
+    edges.forEach(edge => {
+        let v = edge[0], e = edge[1];
         
-//         if(!adjacencyList[v]) adjacencyList[v] = [];
-//         if(!adjacencyList[e]) adjacencyList[e] = [];
+        if(!adjacencyList[v]) adjacencyList[v] = [];
+        if(!adjacencyList[e]) adjacencyList[e] = [];
         
-//         adjacencyList[v].push(e);
-//         adjacencyList[e].push(v);   
-//     })
+        adjacencyList[v].push(e);
+        adjacencyList[e].push(v);   
+    })
     
-    for (let [k,v] of edges) {
-        if(k === start && v === end) result = true;
-         adjacencyList.set(k, [...(adjacencyList.get(k) || []), v])
-         adjacencyList.set(v, [...(adjacencyList.get(v) || []), k])
-    }
+    // for (let [k,v] of edges) {
+    //     if(k === start && v === end) result = true;
+    //      adjacencyList.set(k, [...(adjacencyList.get(k) || []), v])
+    //      adjacencyList.set(v, [...(adjacencyList.get(v) || []), k])
+    // }
     
     while(stack.length) {
         let vertex = stack.pop();
         visited[vertex] = true;    
-        adjacencyList.get(vertex) && adjacencyList.get(vertex).forEach(neighbor => {
+        adjacencyList[vertex] && adjacencyList[vertex].forEach(neighbor => {
             if(!visited[neighbor]) stack.push(neighbor);
         })        
     }    
