e876e9af6bc453401ce164e019e9e7ad3932495e
KirschX <68739701+KirschX@users.noreply.github.com>
2022-12-19 17:52:30 +0900

    Time: 74 ms (95.56%), Space: 43.3 MB (88.31%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reorganizeString = function(S) {
+      let hash = {};
+    for (let c of S) hash[c] = hash[c] + 1 || 1;
+
+    let sort = Object.keys(hash).sort((a,b)=>hash[b] - hash[a]);
+    let res = [];
+    let index = 0;
+
+    // console.log(sort)
+    
+    for (let i = 0;i<sort.length;i++) {
+        let occur = hash[sort[i]];
+        if (occur > parseInt((S.length + 1)/2)) return "";
+        for (let j = 0;j < occur;j++) {
+            
+            if (index >= S.length) index = 1;
+               // console.log(index,j)
+            res[index] = sort[i];
+            index += 2;
+        }
+        // console.log(occur, res)
+    } 
+    return res.join('');
+};
