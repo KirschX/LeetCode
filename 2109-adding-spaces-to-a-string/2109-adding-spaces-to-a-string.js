f476eb4a9b0091e0d1c92a431a0ae7803be39c97
KirschX <68739701+KirschX@users.noreply.github.com>
2023-02-16 22:02:44 +0900

    Time: 317 ms (23.19%), Space: 93.6 MB (37.68%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.js
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.js
@@ -4,21 +4,37 @@
  * @return {string}
  */
 var addSpaces = function(s, spaces) {
-    let left =0;
-    let temp ='';
-    let count =0
-    let right = 0;
-    while(right < s.length){
-
-        if(right === spaces[count]){
-         temp += s.slice(left,right) + " "
-        left = right;
-        count++;
-
-        }
-    right++;
+    let result = ""
+    
+    let count = 0;
+    for (let i =0; i<s.length; i++){
+        if(i === spaces[count]){
+            result +=  " " + s[i]
+            count++;
+        } else result +=  s[i]
     }
-
-temp+=s.slice(left,right)
-return temp
+    
+    return result
+    
+    
 };
+
+// var addSpaces = function(s, spaces) {
+//     let left =0;
+//     let temp ='';
+//     let count =0
+//     let right = 0;
+//     while(right < s.length){
+
+//         if(right === spaces[count]){
+//          temp += s.slice(left,right) + " "
+//         left = right;
+//         count++;
+
+//         }
+//     right++;
+//     }
+
+// temp+=s.slice(left,right)
+// return temp
+// };
