ec0aa7b9e04fbc8b5d96559c52c030d3c71ad40c
KirschX <68739701+KirschX@users.noreply.github.com>
2023-02-16 21:59:08 +0900

    Time: 159 ms (91.30%), Space: 77.3 MB (95.65%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.js
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.js
@@ -4,39 +4,21 @@
  * @return {string}
  */
 var addSpaces = function(s, spaces) {
-//     let map = new Map()
-    
-//     let sArr = s.split("")
+    let left =0;
+    let temp ='';
+    let count =0
+    let right = 0;
+    while(right < s.length){
 
-//     spaces.map((item,index) => {
-//         map.set(item, " " + s[item])
-//     })
-    
-//     sArr.map((item, index) => {
-//         if(!map.get(index)){
-//             map.set(index, item)
-//         }
-//     })
-   
+        if(right === spaces[count]){
+         temp += s.slice(left,right) + " "
+        left = right;
+        count++;
 
-    
-//     let temp = Array.from(map.entries()).sort((a,b) => a[0] -b[0])
-    
-//     let result = temp.reduce((acc,cur)=> acc + cur[1] ,'')
-    
-//     return result
-    
-    let result = ""
-    
-    let count = 0;
-    for (let i =0; i<s.length; i++){
-                if(i === spaces[count]){
-            result = result + " " + s[i]
-            count++;
-        } else result = result + s[i]
+        }
+    right++;
     }
-    
-    return result
-    
-    
+
+temp+=s.slice(left,right)
+return temp
 };
