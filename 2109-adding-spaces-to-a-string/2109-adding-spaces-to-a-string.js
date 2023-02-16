5e79e9f6c2cd9c5358ee698eaeb93d8edbfb898c
KirschX <68739701+KirschX@users.noreply.github.com>
2023-02-16 21:50:35 +0900

    Time: 580 ms (5.80%), Space: 125.7 MB (5.80%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.js
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.js
@@ -4,29 +4,43 @@
  * @return {string}
  */
 var addSpaces = function(s, spaces) {
-    let map = new Map()
+//     let map = new Map()
     
-    let sArr = s.split("")
+//     let sArr = s.split("")
 
-    // sArr.map((item,index) => {
-    //     map.set(item, )
-    // })
+//     spaces.map((item,index) => {
+//         map.set(item, " " + s[item])
+//     })
     
-    spaces.map((item,index) => {
-        map.set(item, " " + s[item])
-    })
-    
-    sArr.map((item, index) => {
-        if(!map.get(index)){
-            map.set(index, item)
-        }
-    })
+//     sArr.map((item, index) => {
+//         if(!map.get(index)){
+//             map.set(index, item)
+//         }
+//     })
    
 
     
-    let temp = Array.from(map.entries()).sort((a,b) => a[0] -b[0])
+//     let temp = Array.from(map.entries()).sort((a,b) => a[0] -b[0])
     
-    let result = temp.reduce((acc,cur)=> acc + cur[1] ,'')
+//     let result = temp.reduce((acc,cur)=> acc + cur[1] ,'')
     
-    return result
+//     return result
+    
+    
+    let map = new Map()
+    
+    let sArr = s.split("")
+    
+    let count = 0;
+    sArr.map((item, index) => {
+        if(index === spaces[count]){
+            map.set(index, " " + item)
+            count++;
+        } else map.set(index, item)
+        
+    })
+    
+    return Array.from(map.values()).join("")
+    
+    
 };
