4199890311747aa03c0cd66abb34288d43d68743
KirschX <68739701+KirschX@users.noreply.github.com>
2023-02-19 01:22:41 +0900

    Time: 51 ms (97.84%), Space: 41.9 MB (71.77%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -1,40 +1,36 @@
-const letterCombinations = function(digits) {
-  // Create a mapping of digit to letters
-  const lettersMap = {
-    "2": ["a", "b", "c"],
-    "3": ["d", "e", "f"],
-    "4": ["g", "h", "i"],
-    "5": ["j", "k", "l"],
-    "6": ["m", "n", "o"],
-    "7": ["p", "q", "r", "s"],
-    "8": ["t", "u", "v"],
-    "9": ["w", "x", "y", "z"]
-  };
-  
-  // Create an array to hold the combinations
-  const combinations = [];
-  
-  // Recursive helper function to generate the combinations
-  const generateCombinations = function(digits, index, combination) {
-    // Base case: if the combination has as many characters as the digits string, add it to the combinations array and return
-    if (index === digits.length) {
-      combinations.push(combination);
-      return;
+var letterCombinations = function(digits) {
+       if (digits.length === 0) {
+        return [];
     }
     
-    // Get the letters corresponding to the current digit
-    const letters = lettersMap[digits[index]];
-    
-    // Recursively generate combinations for each letter
-    for (let i = 0; i < letters.length; i++) {
-      generateCombinations(digits, index + 1, combination + letters[i]);
+    let digitObject = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+    };
+
+    const map = new Map(Object.entries(digitObject));
+    const combinations = [];
+
+    function traverse(n, string) {
+        if (n === digits.length) {
+            combinations.push(string);
+            return;
+        }
+
+        const digit = digits[n];
+        const letters = map.get(digit);
+        for (const letter of letters) {
+            traverse(n + 1, string + letter);
+        }
     }
-  };
-  
-  // Start the recursive generation with an empty string
-  if (digits) {
-    generateCombinations(digits, 0, "");
-  }
-  
-  return combinations;
+
+    traverse(0, "");
+    const uniqueCombinations = [...new Set(combinations)];
+    return uniqueCombinations.sort();
 };
