c409818fbd4deda7c3946c1e2ab14245542da8a6
KirschX <68739701+KirschX@users.noreply.github.com>
2023-03-16 21:40:37 +0900

    Time: 78 ms (60.93%), Space: 45 MB (60.74%) - LeetHub

---
+++ 0046-permutations/0046-permutations.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    
+    let result = []
+    
+    const dfs = (n, pos, arr) => {
+        if(n > nums.length-1) return;
+        
+        // console.log(pos)
+        
+        for(let i = 0; i<nums.length; i++){
+            if(pos.includes(i)) continue;
+            dfs(i, [...pos, i], [...arr, nums[i]])
+        }
+        
+        if(arr.length === nums.length) result.push(arr)
+    }
+    
+  for(let i =0; i<nums.length; i++){
+        dfs(i, [i], [nums[i]])
+  }
+    
+    return result
+};
