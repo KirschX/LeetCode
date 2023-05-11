e5eaf26517a9d0641ec152b37a31ce7a79e01c95
KirschX <68739701+KirschX@users.noreply.github.com>
2023-05-11 23:51:32 +0900

    Time: 62 ms (89.62%), Space: 44.6 MB (62.30%) - LeetHub

---
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {boolean}
+ */
+var checkAlmostEquivalent = function(word1, word2) {
+    
+    let result = true;
+    
+    const map = new Map()
+    
+    word1.split("").forEach((item) => {
+        
+        map.set(item, (map.get(item) || 0) + 1  )
+    })
+    
+    word2.split("").forEach((item) => {
+        map.set(item, (map.get(item) || 0) - 1  )
+    })
+    
+    for (const [k,v] of map.entries()){
+        if(v > 3 || v <-3) result = false
+    }
+    console.log(map)
+    
+    return result
+};
