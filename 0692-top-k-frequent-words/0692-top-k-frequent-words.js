e7304d321979ebb3cf5e495d965f0bbb790969e6
KirschX <68739701+KirschX@users.noreply.github.com>
2023-05-15 18:40:04 +0900

    Time: 82 ms (41.53%), Space: 47.8 MB (25.59%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.js
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -1,13 +1,25 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
 var topKFrequent = function(words, k) {
-    let hash = {};
-    for (let word of words) {
-        hash[word] = hash[word]+1||1;
-    }
-    let result = Object.keys(hash).sort((a,b)=>{
-            let countCompare = hash[b] - hash[a];
-            if (countCompare == 0) return a.localeCompare(b);
-            else return countCompare;
-        }   
-    );
-    return result.slice(0, k);
+    
+    const map = new Map();
+    
+    words.forEach((item) => {
+        map.set(item, (map.get(item) || 0) + 1 )
+    })
+    
+    // console.log(map)
+    
+    const collator = new Intl.Collator('en', { numeric: true, sensitivity: 'base' })
+    
+    const result = Array.from(map.entries()).sort((a,b) => {
+        if(a[1] === b[1]) return collator.compare(a[0],b[0])
+        else return b[1] - a[1]
+    })
+    
+    return result.slice(0,k).map(([k,v]) => k)
 };
+
