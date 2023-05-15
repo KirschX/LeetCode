6220238b1c9ab978ee6d7b70d6838b5ecd0f86a2
KirschX <68739701+KirschX@users.noreply.github.com>
2023-05-15 18:35:48 +0900

    Time: 662 ms (5.42%), Space: 52.5 MB (5.08%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+    
+    const map = new Map();
+    
+    words.forEach((item) => {
+        map.set(item, (map.get(item) || 0) + 1 )
+    })
+    
+    // console.log(map)
+    
+    const result = Array.from(map.entries()).sort((a,b) => {
+        if(a[1] === b[1]) return Intl.Collator().compare(a[0],b[0])
+        else return b[1] - a[1]
+    })
+    
+    return result.slice(0,k).map(([k,v]) => k)
+};
