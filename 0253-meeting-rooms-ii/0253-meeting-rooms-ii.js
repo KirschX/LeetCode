5a27cfd6d7abc75e4b67b4e72a55e8391d54473f
KirschX <68739701+KirschX@users.noreply.github.com>
2023-06-10 14:49:31 +0900

    Time: 85 ms (36.42%), Space: 46 MB (31.50%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.js
@@ -3,21 +3,49 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    const map = new Map();
-    let count = 0;
+    class MinPQ {
+        constructor() {
+            this.compare = (a,b) => a - b
+            this.heap = [];
+        }
+        
+        enque(v) {
+            this.heap.push(v);
+            this.heap.sort(this.compare)
+       
+        }
+        
+        peek() {
+            if(this.size === 0) return null;
+            return this.heap[0]
+        }
+        
+        deque() {
+            if (this.size === 0) return null;
+            this.heap.shift();
+      
+            return;
+        }
+        
+        get size() {
+            return this.heap.length;
+        }
+    }
+    
+    const minPQ = new MinPQ();
     let max = 0;
     
-      intervals.forEach(([start,end],i) => {
-          map.set(start, (map.get(start) || 0) + 1)
-          map.set(end, (map.get(end) || 0) + -1)
-      })
+    intervals.sort((a,b) => a[0] - b[0])
     
-    const entryArr = Array.from(map.entries()).sort((a,b) => a[0] - b[0])
-    
-    entryArr.map((item, i) => {
-        count = count + item[1]
-        max = Math.max(max, count)
+    intervals.forEach(([start,end]) => {
+        
+        if(minPQ.size > 0 && start >= minPQ.peek()){
+            minPQ.deque();
+        }
+        minPQ.enque(end);
+        max = Math.max(max, minPQ.size)
     })
+
     
     return max;
 };
