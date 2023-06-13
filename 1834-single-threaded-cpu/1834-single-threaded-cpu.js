3e2cfc00ca27b85a8545cdf6aedc57f82a922694
KirschX <68739701+KirschX@users.noreply.github.com>
2023-06-13 20:00:33 +0900

    Time: 834 ms (26.32%), Space: 113.5 MB (47.37%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.js
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.js
@@ -1,17 +1,41 @@
-var getOrder = function(A) {
-    A=A.map(([a,b],i)=>[a,b,i]).sort((a,b)=>b[0]-a[0])
-    let pq = new MinPriorityQueue({ priority: ([e,p,idx])=>p+10**(-7)*(idx+1) }),
-         curtime=A[A.length-1][0],res=[]
-    while(A.length||pq.size()){
-        while(A.length&&curtime>=A[A.length-1][0]) //get everything available inside the pq
-            pq.enqueue(A.pop())
-        if(pq.size()){ //make the selection
-            let [e,p,idx]=pq.dequeue()['element']
-            curtime+=Number(p)
-            res.push(idx)
+/**
+ * @param {number[][]} tasks
+ * @return {number[]}
+ */
+
+var getOrder = function(tasks) {
+    const mpq = new MinPriorityQueue({
+        compare: (a, b) => { // Compare function
+            if(a[1] === b[1]) return a[2] - b[2]; 
+            return a[1] - b[1]; // Sort by processing time
         }
-        else if(A.length) //otherwise, increment time for the next element. 
-            curtime=A[A.length-1][0]
+    });
+    tasks = tasks.map(([a,b],i)=>[a,b,i]).sort((a,b)=>a[0]-b[0] || a[1] - b[1])
+    
+    let res = [];
+    let curTime = tasks[0][0];
+    let i = 0;
+    
+    
+    while(mpq.size() || i < tasks.length ) {
+        // 시작 시점이 같은 task, Available tasks에 enque
+        while(i < tasks.length && curTime >= tasks[i][0]) {
+            mpq.enqueue(tasks[i]);
+            i++;
+        }
+        // mpq에 실행할 task가 남았다면 실행
+        if(mpq.size()){
+            const temp = mpq.dequeue();
+            res.push(temp[2]);
+            curTime = curTime + temp[1];
+        // 실행할 task가 없다면 다음 task 시간으로 넘어가기
+        } else {
+            curTime = tasks[i][0];
+        }
+        
+        
     }
-    return res
+
+
+    return res;
 };
