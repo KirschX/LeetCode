24c22a4c866f3bd983b79f5d59c9cb175a186313
KirschX <68739701+KirschX@users.noreply.github.com>
2023-06-13 20:00:01 +0900

    Time: 668 ms (68.42%), Space: 113.5 MB (47.37%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.js
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.js
@@ -2,36 +2,40 @@
  * @param {number[][]} tasks
  * @return {number[]}
  */
+
 var getOrder = function(tasks) {
-    let res = [];
     const mpq = new MinPriorityQueue({
         compare: (a, b) => { // Compare function
-            if(a[1] === b[1]) return a[2] - b[2]; // Compare by index if processing time is equal
+            if(a[1] === b[1]) return a[2] - b[2]; 
             return a[1] - b[1]; // Sort by processing time
         }
     });
+    tasks = tasks.map(([a,b],i)=>[a,b,i]).sort((a,b)=>a[0]-b[0] || a[1] - b[1])
     
-    tasks = tasks.map(([a, b], index) => [a, b, index]);
-    tasks.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
-
-     let currentProcessingTime = tasks[0][0]; 
-     let i = 0;
-    //
-    while(mpq.size() || i < tasks.length){
-        while(i < tasks.length && currentProcessingTime >= tasks[i][0]) { // Check if task is able for processing
-            mpq.enqueue(tasks[i]); // Add task based on its comparison with other values in the queue in O(log(n)) runtime
-            i++; // Increment i
+    let res = [];
+    let curTime = tasks[0][0];
+    let i = 0;
+    
+    
+    while(mpq.size() || i < tasks.length ) {
+        // 시작 시점이 같은 task, Available tasks에 enque
+        while(i < tasks.length && curTime >= tasks[i][0]) {
+            mpq.enqueue(tasks[i]);
+            i++;
         }
+        // mpq에 실행할 task가 남았다면 실행
         if(mpq.size()){
-            const task = mpq.dequeue();
-            res.push(task[2]);
-            currentProcessingTime += task[1];
+            const temp = mpq.dequeue();
+            res.push(temp[2]);
+            curTime = curTime + temp[1];
+        // 실행할 task가 없다면 다음 task 시간으로 넘어가기
         } else {
-            currentProcessingTime = tasks[i][0];
+            curTime = tasks[i][0];
         }
+        
+        
     }
+
 
     return res;
 };
-
-//
