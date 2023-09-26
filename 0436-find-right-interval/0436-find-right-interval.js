4a952b68babf9a0e74bfe4f5b314022bc43f080e
KirschX <68739701+KirschX@users.noreply.github.com>
2023-09-26 16:18:48 +0900

    Time: 108 ms (50.00%), Space: 60.4 MB (6.78%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[]}
+ */
+var findRightInterval = function(intervals) {
+    
+    intervals = intervals.map((item,index) => [...item, index]);
+    
+    intervals.sort((a,b) => a[0] - b[0])
+    
+    let result = Array.from({length: intervals.length}).map((item) => "_");
+    
+    function search(arr, left, right, tar) {
+        
+        let mid;
+        while(left <= right){
+            mid = Math.floor((left + right)/2);
+            if(arr[mid][0] < tar) left = mid + 1;
+            else right = mid - 1;
+        }
+        
+        return intervals[left];
+    }
+    
+
+    for(let i =0; i<intervals.length; i++){
+        const item = intervals[i];
+        const target = intervals[i][1]
+        let rightInterval = search(intervals, 0, intervals.length-1, target)
+
+        
+        if(rightInterval) result[item[2]] = rightInterval[2]
+        else result[item[2]] = -1
+    }
+
+   
+    return result
+};
