9290c0d1e5c8418525aa534604d0398f8f591548
KirschX <68739701+KirschX@users.noreply.github.com>
2023-12-18 02:40:35 +0900

    Time: 70 ms (27.19%), Space: 44 MB (70.88%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color) {
+    
+    const n = image.length;
+    const m = image[0].length;
+    const target = image[sr][sc]
+    
+    const visited = new Array(n).fill(0).map((item) => new Array(m).fill(0));
+
+    
+    function dfs(x,y) {
+        if(x>=n || y>=m || x<0 || y<0) return;
+        if(visited[x][y] === 1 || image[x][y] !== target) return;
+
+        visited[x][y] = 1;
+        image[x][y] = color;
+
+        
+        
+         dfs(x+1,y)
+         dfs(x,y-1)
+       dfs(x-1, y)
+        dfs(x, y+1)
+
+    }
+    
+    dfs(sr,sc);
+
+    return image
+};
