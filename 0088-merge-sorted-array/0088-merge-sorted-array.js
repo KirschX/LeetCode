f432fda22dc7204762492dfac292bef990bafd4e
KirschX <68739701+KirschX@users.noreply.github.com>
2024-11-17 18:27:27 +0900

    Time: 6 ms (6.82%), Space: 52.7 MB (5.15%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,27 +6,12 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    // Start from the end of nums1 and nums2
-    let i = m - 1; // Last element of nums1's valid part
-    let j = n - 1; // Last element of nums2
-    let k = m + n - 1; // Last position in nums1
+    
 
-    // Merge nums1 and nums2 starting from the end
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            i--;
-        } else {
-            nums1[k] = nums2[j];
-            j--;
-        }
-        k--;
-    }
-
-    // // If any elements remain in nums2, copy them to nums1
-    while (j >= 0) {
-        nums1[k] = nums2[j];
-        j--;
-        k--;
-    }
+    nums1.splice(m, n, ...nums2)
+    
+    nums1.sort((a,b)=> a-b)
+    
+    console.log(nums1)
+    
 };
